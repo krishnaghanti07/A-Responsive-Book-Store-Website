d2e434245f93c33b5de23916f9e3c89e7fc01c18
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2024-04-03 03:30:00 +0530

    Added and Completed an Blog Section

--- js/script.js
+++ js/script.js
@@ -137,3 +137,28 @@ var swiper = new Swiper(".reviews-slider", {
       },
     },
   });
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20 ,
+    grabCursor: true ,
+    loop: true ,
+    centeredSlides: true ,
+    autoplay: {
+        delay: 9500 ,
+        disableOnInteraction: false ,
+    } ,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        // spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        // spaceBetween: 20,
+      },
+    },
+  });
